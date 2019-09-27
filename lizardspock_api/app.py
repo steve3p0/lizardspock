19b1e66d15bbb3ae19bcf1316cb5a8c3b642ff0b
steve3p0 <sbraich@bpdx.edu>
2019-09-27 11:38:32 -0700

    need request/reponse error handling.

--- lizardspock_api/app.py
+++ lizardspock_api/app.py
@@ -1,48 +1,43 @@
 # Rock Paper Scissors Lisard Spock
+# By Steve Braich
 #
-# https://www.youtube.com/watch?v=R0pUbct9WgI
-from collections import namedtuple
-from typing import Dict, List, Union, NewType
-from flask import Flask, jsonify, request
-from flask_restful import reqparse, Resource, Api, fields, marshal, marshal_with, marshal_with_field
+# Inspiration:
+#   Flask RESTful Tutorial          https://flask-restful.readthedocs.io/en/0.3.5/quickstart.html
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+from typing import List
 import lizardspock
-import random
-import json
 
 app = Flask(__name__)
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('player_choice', type=int)
-
 class Choices(Resource):
 
-    def get(self):
-        #return jsonify(choices)
+    def get(self) -> List:
         return lizardspock.choices
 
 
 class Choice(Resource):
 
-    def get(self):
-        #return jsonify(get_random_choice())
+    def get(self) -> dict:
         return lizardspock.get_random_choice()
 
 
 class Play(Resource):
 
-    def post(self):
-        args = parser.parse_args()
-        player_choice = int(args['player_choice'])
-        computer_choice = lizardspock.get_random_choice()['id']
-        result = lizardspock.play(player_choice, computer_choice)
-        return result
-        #return jsonify(result)
+    def post(self) -> List:
+        json_data = request.get_json()
+        player = json_data['player']
 
+        computer = lizardspock.get_random_choice()['id']
+        result = lizardspock.play(player, computer)
+        return result
 
 api.add_resource(Choices, '/choices')
 api.add_resource(Choice, '/choice')
 api.add_resource(Play, '/play')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- lizardspock_api/lizardspock.py
+++ lizardspock_api/lizardspock.py
@@ -1,25 +1,67 @@
-# Rock Paper Scissors Lisard Spock
+# Rock Paper Scissors Lizard Spock
+# By Steve Braich
 #
-# https://www.youtube.com/watch?v=R0pUbct9WgI
+#   The Code Challenge              https://codechallenge.boohma.com/
+#   The Big Bang Theory Episode     https://www.youtube.com/watch?v=YxVgzNYP45I
+
 from collections import namedtuple
-from typing import Dict, List, Union, NewType
+from typing import Dict
 import random
 
 Results = namedtuple('Results', 'win lose tie')
 results = Results('win', 'lose', 'tie')
+choices = [{'id': 1, 'name': 'rock'},
+           {'id': 2, 'name': 'spock'},
+           {'id': 3, 'name': 'paper'},
+           {'id': 4, 'name': 'lizard'},
+           {'id': 5, 'name': 'scissors'}]
 
-ChoiceDict = NewType('ChoiceType', Dict[int, str])
 
-choices: List[ChoiceDict] = \
-            [
-               {'id': 0, 'name': 'rock'},
-               {'id': 1, 'name': 'paper'},
-               {'id': 2, 'name': 'scissors'},
-               {'id': 3, 'name': 'lizard'},
-               {'id': 4, 'name': 'spock'}
-            ]
+def get_random_choice() -> Dict:
+    upper = len(choices)
+    random_c = random.randrange(1, upper)
+    choice = [c for c in choices if c['id'] == random_c][0]
+    return choice
 
-def play(player, computer):
+
+def validate_choice(choice_id: int) -> None:
+    if not isinstance(choice_id, int):
+        raise TypeError(f"Invalid choice type. Expected 'int' got '{type(choice_id)}' instead.")
+
+    lower = 1
+    upper = len(choices)
+
+    if not lower <= choice_id <= upper:
+        raise IndexError(f"{choice_id} is an invalid choice id. Expecting 'int' between {lower} and {upper}.")
+
+
+def play(player: int, computer: int) -> Dict:
+
+    # Validate player and computer are existing choices
+    validate_choice(player)
+    validate_choice(computer)
+
+    if computer % 5 == player - 1:
+        result = results.win
+    elif (computer + 1) % 5 == player - 1:
+        result = results.win
+    elif computer == player:
+        result = results.tie
+    else:
+        result = results.lose
+
+    res = {'results': result, 'player': player, 'computer': computer}
+    return res
+
+def play1(player: int, computer: int) -> Dict:
+
+    # Validate player and computer are existing choices
+    validate_choice(player)
+    validate_choice(computer)
+
+    player -= 1
+    computer -= 1
+
     if (computer + 1) % 5 == player:
         result = results.win
     elif (computer + 2) % 5 == player:
@@ -29,11 +71,5 @@ def play(player, computer):
     else:
         result = results.lose
 
-    return {'results': result, 'player': player, 'computer': computer }
-
-
-def get_random_choice():
-    upper = len(choices)
-    random_c = random.randrange(1, upper)
-    choice = [c for c in choices if c['id'] == random_c][0]
-    return choice
+    res = {'results': result, 'player': player + 1, 'computer': computer + 1}
+    return res
