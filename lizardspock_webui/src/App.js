dff6698384d22ccb7024cafa7edc7925f38d250d
steve3p0 <sbraich@bpdx.edu>
2019-09-27 12:26:19 -0700

    initial

--- lizardspock_webui/src/App.js
+++ lizardspock_webui/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, {Component} from 'react';
+import Languages from './components/languages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+    state = { languages: [] }
+
+    componentDidMount()
+    {
+        fetch('http://127.0.0.1:5000/language', )
+        //fetch('http://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then((data) => { this.setState({ languages: data })})
+            .catch(console.log)
+      }
+
+    render ()
+    {
+        return (
+            <Languages languages={this.state.languages} />
+        );
+    }
 }
 
 export default App;
